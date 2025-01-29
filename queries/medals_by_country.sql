4ed06b60fc48242bd110490b9a8139c6224f116c
RitvikaK. <ritvika.kedia@icloud.com>
2025-01-29 15:12:39 +0000

    Update medals_by_country.sql

--- queries/medals_by_country.sql
+++ queries/medals_by_country.sql
@@ -1,1 +1,7 @@
-
+with gold AS (SELECT noc, count(medal)gold_medal from athlete_events where lower(medal)='gold' group by noc),
+silver AS (SELECT noc, count(medal)silver_medal from athlete_events where lower(medal)='silver' GROUP by noc),
+bronze AS (SELECT noc, count(medal)bronze_medal from athlete_events where lower(medal)='bronze' group by noc),
+noc_medals AS (SELECT * from gold LEFT JOIN (SELECT * from silver) using (noc) LEFT JOIN (SELECT * from bronze) using (noc))
+SELECT region, sum(gold_medal) gold,sum( silver_medal)silver, sum(bronze_medal) bronze from noc_medals LEFT JOIN (SELECT * from noc_regions) using(noc)
+group by region
+ORDER BY region
