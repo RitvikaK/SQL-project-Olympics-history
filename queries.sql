5776ec014e5e977cbb5a8ee15fecc147ee114bc7
RitvikaK. <ritvika.kedia@icloud.com>
2025-01-29 15:08:43 +0000

    Delete queries.sql

--- queries.sql
+++
@@ -1,9 +0,0 @@
-queries/
-│── 📝 nations_all_olympics.sql  
-│── 📝 sport_all_summer_olympics.sql  
-│── 📝 medals_by_country.sql  
-│── 📝 most_medals_each_olympics.sql  
-│── 📝 most_medals_total_each_olympics.sql  
-│── 📝 countries_without_gold.sql  
-│── 📝 india_highest_medals_sport.sql  
-│── 📝 india_hockey_medals_breakdown.sql  
