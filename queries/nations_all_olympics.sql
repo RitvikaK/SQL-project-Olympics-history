d33947b296eec773c50ed77f02b44b89af1fd9c7
RitvikaK. <ritvika.kedia@icloud.com>
2025-01-29 15:11:42 +0000

    Update nations_all_olympics.sql

--- queries/nations_all_olympics.sql
+++ queries/nations_all_olympics.sql
@@ -1,1 +1,6 @@
-
+WITH nation AS (SELECT * from athlete_events LEFT JOIN (SELECT * from noc_regions) using (noc)),
+country_counts AS (SELECT region, count (distinct games)count  from nation
+GROUP BY region
+—ORDER BY count desc),
+total_games AS (SELECT count(distinct games) games_count from athlete_events)
+SELECT region from country_counts WHERE count IN (SELECT games_count from total_games)
