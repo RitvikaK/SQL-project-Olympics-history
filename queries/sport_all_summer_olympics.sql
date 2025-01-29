0dc16e400da3637a6cd4c814f7cc407bb4a02c53
RitvikaK. <ritvika.kedia@icloud.com>
2025-01-29 15:12:16 +0000

    Update sport_all_summer_olympics.sql

--- queries/sport_all_summer_olympics.sql
+++ queries/sport_all_summer_olympics.sql
@@ -1,1 +1,5 @@
-
+WITH summer AS (SELECT * from athlete_events where season LIKE 'Summer'),
+games AS (SELECT count(distinct games) summer_games from summer),
+sport AS (SELECT sport, count(distinct games) count from summer)
+SELECT sport from sport
+WHERE count=(SELECT summer_games from games)
